f09a4d041262cdbd3fc71972a161bb4fd03242de
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-15 22:42:24 -0500

    Edit Customer

--- src/main/java/com/kharoud/cars/controllers/HomeController.java
+++ src/main/java/com/kharoud/cars/controllers/HomeController.java
@@ -6,7 +6,9 @@ import com.kharoud.cars.models.Customer;
 import com.kharoud.cars.repository.CarsRepository;
 import com.kharoud.cars.repository.CartRepository;
 import com.kharoud.cars.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
@@ -18,6 +20,7 @@ public class HomeController {
 
     private Cart cart;
     private Customer customer;
+
 
     private CarsRepository carsRepository;
     private CustomerRepository customerRepository;
@@ -39,60 +42,83 @@ public class HomeController {
         }
     }
 
-    @PostMapping(value = "/create", consumes = "application/json")
-    @ResponseBody
-    public Customer create( Customer customer ) {
+    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
+    public Customer create(@RequestBody Customer customer ) {
 
         customerRepository.save( customer );
         return customer;
     }
 
-    @RequestMapping("/update")
-    public List<Cart> home() {
+    @GetMapping(value = "/edit/{id}", produces = "application/json")
+    @ResponseBody
+    public Customer Edit(Model model, @PathVariable("id") String id) {
 
-        customer = new Customer();
-        customer.setEmail("Dskharoud3@gmail.com");
-        customer.setName("Kharoud");
-        customerRepository.save(customer);
-
-        Cars cars = new Cars();
-        cars.setName("Mustang");
-        cars.setColor("Black");
-        cars.setEngine("High power");
-        cars.setPrice(1000);
-
-
-        Cars cars1 = new Cars();
-        cars1.setName("Challenger");
-        cars1.setColor("Black");
-        cars1.setEngine("High power");
-        cars1.setPrice(1000);
-
-        carsRepository.save(cars);
-        carsRepository.save(cars1);
-
-        cart = new Cart();
-        cart.setCustomer(customer);
-        cart.getCarsList().add(cars);
-        cart.getCarsList().add(cars1);
-
-        cartRepository.save(cart);
-
-        return cartRepository.findAll();
+        Customer cust = customerRepository.findById(Integer.valueOf( id )).orElse(null);
+        model.addAttribute("customer", cust );
+        return cust;
     }
 
-    @RequestMapping("/cart")
-    public List<Cart> getCart() {
+    @GetMapping(value = "/delete/{id}", produces = "application/json")
+    @ResponseBody
+    public Customer deleteCust (Model model, @PathVariable("id") String id) {
 
-        return cartRepository.findAll();
+        Customer cust = customerRepository.findById(Integer.valueOf( id )).orElse(null);
+        customerRepository.deleteById( Integer.valueOf( id ));
+        return cust;
     }
 
-    @RequestMapping("/cust")
+
+//
+//    @RequestMapping("/update")
+//    public List<Cart> home() {
+//
+//        customer = new Customer();
+//        customer.setEmail("Dskharoud3@gmail.com");
+//        customer.setName("Kharoud");
+//        customerRepository.save(customer);
+//
+//        Cars cars = new Cars();
+//        cars.setName("Mustang");
+//        cars.setColor("Black");
+//        cars.setEngine("High power");
+//        cars.setPrice(1000);
+//
+//
+//        Cars cars1 = new Cars();
+//        cars1.setName("Challenger");
+//        cars1.setColor("Black");
+//        cars1.setEngine("High power");
+//        cars1.setPrice(1000);
+//
+//        carsRepository.save(cars);
+//        carsRepository.save(cars1);
+//
+//        cart = new Cart();
+//        cart.setCustomer(customer);
+//        cart.getCarsList().add(cars);
+//        cart.getCarsList().add(cars1);
+//
+//        cartRepository.save(cart);
+//
+//        return cartRepository.findAll();
+//    }
+//
+//    @RequestMapping("/cart")
+//    public List<Cart> getCart() {
+//
+//        return cartRepository.findAll();
+//    }
+
+    @RequestMapping(value = "/cust", produces = "application/json")
+    @ResponseBody
     public List<Customer> getCust() {
 
-
+        customer = new Customer();
         customer.setName("asdf");
         customer.setEmail("asdf");
+        customer.setUsername("sunny");
+        customer.setPassword("1");
+        customer.setPhonenumber("1241241");
         customerRepository.save(customer);
 
         return customerRepository.findAll();
--- src/main/java/com/kharoud/cars/repository/CustomerRepository.java
+++ src/main/java/com/kharoud/cars/repository/CustomerRepository.java
@@ -11,4 +11,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
        Customer findByUsernameEqualsAndPasswordEquals(String username, String password);
 
+
 }
